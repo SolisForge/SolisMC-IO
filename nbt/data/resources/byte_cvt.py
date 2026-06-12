f29a7076e53756f8680644c14e70ef0ad1ada00b
Meltwin <github@meltwin.fr>
2026-06-12 19:39:51 +0200

    SOLISMC-IO #8: Added stream export

--- nbt/data/resources/byte_cvt.py
+++ nbt/data/resources/byte_cvt.py
@@ -12,5 +12,17 @@
 import struct
 
 # =============================================================================
-def int_to_bytes(v: int, fmt: str) -> bytes:
+def list_to_bytes(values: list, fmt: str) -> bytes:
+    b = b""
+    for value in values:
+        b += value_to_bytes(value, fmt)
+    return b
+
+# =============================================================================
+def value_to_bytes(v: int, fmt: str) -> bytes:
     return struct.pack(f">{fmt}", v)
+
+# =============================================================================
+def byte_to_array(b_t: bytes) -> str:
+    return ','.join([f"\'\\x{b:>02x}\'" for b in b_t])
+
--- nbt/data/views/value_struct.hpp
+++ nbt/data/views/value_struct.hpp
@@ -1,6 +1,6 @@
 
-struct ${name} {
-  static constexpr auto VALUE{ ${value} };
+struct ${name_upper} {
+  static constexpr ${value_type} VALUE{ ${value} };
   static constexpr char BYTES[${n_bytes}] = { ${bytes_str} };
   static constexpr std::size_t N_BYTES{${n_bytes}};
 };
