13236c415cf0aa6fb33f39a3654161f76ce7c305
Meltwin <github@meltwin.fr>
2026-06-16 09:24:17 +0200

    SOLISMC-IO #2: Added bedrock specialization to main header

--- nbt/include/minecraft/io/nbt/bytes.hpp
+++ nbt/include/minecraft/io/nbt/bytes.hpp
@@ -15,14 +15,29 @@
 #include "minecraft/io/nbt/bytes/float.hpp"    // IWYU pragma: keep
 #include "minecraft/io/nbt/bytes/integral.hpp" // IWYU pragma: keep
 #include "minecraft/io/nbt/bytes/interface.hpp"
+#include "minecraft/io/nbt/bytes/list.hpp"   // IWYU pragma: keep
 #include "minecraft/io/nbt/bytes/string.hpp" // IWYU pragma: keep
 
 namespace minecraft::nbt {
 
+// ------------------------------------------------------------------
+// Default = JAVA specific NBT classes
+// ------------------------------------------------------------------
 template <typename T>
 using ByteParser = byte::base::ByteParser<T, GameVersion::JAVA>;
 template <typename T>
 using ByteWriter = byte::base::ByteWriter<T, GameVersion::JAVA>;
+
+// ------------------------------------------------------------------
+// Bedrock specific NBT classes
+// ------------------------------------------------------------------
+namespace bedrock {
+
+template <typename T>
+using ByteParser = byte::base::ByteParser<T, GameVersion::BEDROCK>;
+template <typename T>
+using ByteWriter = byte::base::ByteWriter<T, GameVersion::BEDROCK>;
+} // namespace bedrock
 
 } // namespace minecraft::nbt
 
