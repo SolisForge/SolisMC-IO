04f8317f8c82bb17f2663065b8b2cd2eb59de87e
Meltwin <github@meltwin.fr>
2026-06-30 09:21:50 +0200

    SOLISMC-IO #5: Added unit tests for short, long & byte writers

--- nbt/tests/bytes/0_byte.cpp
+++ nbt/tests/bytes/0_byte.cpp
@@ -11,16 +11,21 @@
 // ============================================================================
 
 #include "solismc_io/dataset/0_byte.hpp"
+#include "common.hpp"
 #include "minecraft/io/nbt/bytes.hpp" // IWYU pragma: keep
+#include <array>
 #include <cstdint>
 #include <doctest/doctest.h>
 
 using namespace minecraft::nbt;
+using INT_TYPE = int8_t;
+constexpr std::size_t TYPE_SIZE{sizeof(INT_TYPE)};
+constexpr auto INCOMPLETE_OFFSET{1};
 
 // ============================================================================
 TEST_CASE("BytesParser<NBT::Byte>") {
 
-  ByteParser<int8_t> parser;
+  ByteParser<INT_TYPE> parser;
 
   //  --------------------------------------------------------------------------
   SUBCASE("[BYTE1] Normal case") {
@@ -55,7 +60,7 @@ TEST_CASE("BytesParser<NBT::Byte>") {
     CHECK_EQ(n, 0);
   }
   //  --------------------------------------------------------------------------
-  SUBCASE("[STREAM_BYTE1] Two ints") {
+  SUBCASE("[STREAM_BYTE1] Two bytes") {
     auto *p = STREAM_BYTE1::STREAM;
     auto n = STREAM_BYTE1::N_BYTES;
 
@@ -63,20 +68,21 @@ TEST_CASE("BytesParser<NBT::Byte>") {
       auto ret = parser.parse(p, n);
       CHECK_EQ(ret, ParseResult::ENDED);
       CHECK_EQ(parser.get(), STREAM_BYTE1::VALUES[i]);
-      CHECK_EQ(n, STREAM_BYTE1::N_BYTES - sizeof(int8_t) * (i + 1));
+      CHECK_EQ(n, STREAM_BYTE1::N_BYTES - TYPE_SIZE * (i + 1));
     }
   }
   //  --------------------------------------------------------------------------
   SUBCASE("[STREAM_BYTE1] Incomplete stream") {
     auto *p = STREAM_BYTE1::STREAM;
     // We induce an incomplete stream
-    auto n = STREAM_BYTE1::N_BYTES - 1;
+    const auto STRM_LEN = STREAM_BYTE1::N_BYTES - INCOMPLETE_OFFSET;
+    auto n = STRM_LEN;
 
     for (std::size_t i = 0; i < STREAM_BYTE1::N_VALUES - 1; i++) {
       auto ret = parser.parse(p, n);
       CHECK_EQ(ret, ParseResult::ENDED);
       CHECK_EQ(parser.get(), STREAM_BYTE1::VALUES[i]);
-      CHECK_EQ(n, STREAM_BYTE1::N_BYTES - 1 - sizeof(int8_t) * (i + 1));
+      CHECK_EQ(n, STRM_LEN - TYPE_SIZE * (i + 1));
     }
 
     // Incomplete parsing
@@ -84,5 +90,83 @@ TEST_CASE("BytesParser<NBT::Byte>") {
     CHECK_EQ(ret, ParseResult::UNFINISHED);
     CHECK_EQ(parser.get(), 0);
     CHECK_EQ(n, 0);
+  }
+}
+
+// ============================================================================
+TEST_CASE("BytesWriter<NBT::Byte>") {
+
+  ByteWriter<INT_TYPE> dumper;
+  std::array<char, TYPE_SIZE> buffer;
+
+  //  --------------------------------------------------------------------------
+  SUBCASE("[BYTE1] Normal case") {
+    dumper.set(BYTE1::VALUE);
+    auto p = buffer.data();
+    std::size_t n = BYTE1::N_BYTES;
+
+    auto ret = dumper.write(p, n);
+    check_writing_ended(ret, n);
+    buffers_are_equal(buffer, BYTE1::BYTES, TYPE_SIZE);
+  }
+  //  --------------------------------------------------------------------------
+  SUBCASE("[BYTE2] Negative case") {
+    dumper.set(BYTE2::VALUE);
+    auto p = buffer.data();
+    auto n = BYTE2::N_BYTES;
+    auto ret = dumper.write(p, n);
+    check_writing_ended(ret, n);
+    buffers_are_equal(buffer, BYTE2::BYTES, TYPE_SIZE);
+  }
+  // --------------------------------------------------------------------------
+  SUBCASE("[BYTE3] Big value case") {
+    dumper.set(BYTE3::VALUE);
+    auto p = buffer.data();
+    auto n = BYTE3::N_BYTES;
+    auto ret = dumper.write(p, n);
+    check_writing_ended(ret, n);
+    buffers_are_equal(buffer, BYTE3::BYTES, TYPE_SIZE);
+  }
+  // --------------------------------------------------------------------------
+  SUBCASE("[STREAM_BYTE1] Two bytes") {
+    std::array<char, STREAM_BYTE1::N_BYTES> long_buffer;
+    auto *p = long_buffer.data();
+    auto n = STREAM_BYTE1::N_BYTES;
+
+    for (std::size_t i = 0; i < STREAM_BYTE1::N_VALUES; i++) {
+      dumper.set(STREAM_BYTE1::VALUES[i]);
+
+      auto ret = dumper.write(p, n);
+      CHECK_EQ(ret, WriteResult::ENDED);
+      CHECK_EQ(n, STREAM_BYTE1::N_BYTES - TYPE_SIZE * (i + 1));
+    }
+    buffers_are_equal(long_buffer, STREAM_BYTE1::STREAM, STREAM_BYTE1::N_BYTES);
+  }
+  // --------------------------------------------------------------------------
+  SUBCASE("[STREAM_BYTE1] Incomplete stream") {
+    std::array<char, STREAM_BYTE1::N_BYTES> long_buffer;
+    auto *p = long_buffer.data();
+    // We induce an incomplete stream
+    const auto STRM_LEN = STREAM_BYTE1::N_BYTES - INCOMPLETE_OFFSET;
+    auto n = STRM_LEN;
+
+    for (std::size_t i = 0; i < STREAM_BYTE1::N_VALUES - 1; i++) {
+      dumper.set(STREAM_BYTE1::VALUES[i]);
+      auto ret = dumper.write(p, n);
+      CHECK_EQ(ret, WriteResult::ENDED);
+      CHECK_EQ(n, STRM_LEN - TYPE_SIZE * (i + 1));
+    }
+
+    // Incomplete parsing
+    dumper.set(STREAM_BYTE1::VALUES[STREAM_BYTE1::N_VALUES - 1]);
+    auto ret = dumper.write(p, n);
+    CHECK_EQ(ret, WriteResult::UNFINISHED);
+    CHECK_EQ(n, 0);
+
+    // Check buffers
+    for (std::size_t i = 0; i < STRM_LEN; i++)
+      CHECK_EQ(long_buffer[i], STREAM_BYTE1::STREAM[i]);
+    for (uint8_t i = STRM_LEN; i < STREAM_BYTE1::N_VALUES * TYPE_SIZE; i++)
+      CHECK_EQ(long_buffer[i], 0);
   }
 }
--- nbt/tests/bytes/0_long.cpp
+++ nbt/tests/bytes/0_long.cpp
@@ -11,16 +11,21 @@
 // ============================================================================
 
 #include "solismc_io/dataset/0_long.hpp"
+#include "common.hpp"
 #include "minecraft/io/nbt/bytes.hpp" // IWYU pragma: keep
+#include <array>
 #include <cstdint>
 #include <doctest/doctest.h>
 
 using namespace minecraft::nbt;
+using INT_TYPE = int64_t;
+constexpr std::size_t TYPE_SIZE{sizeof(INT_TYPE)};
+constexpr auto INCOMPLETE_OFFSET{1};
 
 // ============================================================================
 TEST_CASE("BytesParser<NBT::Long>") {
 
-  ByteParser<int64_t> parser;
+  ByteParser<INT_TYPE> parser;
 
   //  --------------------------------------------------------------------------
   SUBCASE("[LONG1] Normal case") {
@@ -54,7 +59,7 @@ TEST_CASE("BytesParser<NBT::Long>") {
     CHECK_EQ(n, 0);
   }
   //  --------------------------------------------------------------------------
-  SUBCASE("[STREAM_LONG1] Two shorts") {
+  SUBCASE("[STREAM_LONG1] Two longs") {
     auto *p = STREAM_LONG1::STREAM;
     auto n = STREAM_LONG1::N_BYTES;
 
@@ -62,20 +67,21 @@ TEST_CASE("BytesParser<NBT::Long>") {
       auto ret = parser.parse(p, n);
       CHECK_EQ(ret, ParseResult::ENDED);
       CHECK_EQ(parser.get(), STREAM_LONG1::VALUES[i]);
-      CHECK_EQ(n, STREAM_LONG1::N_BYTES - sizeof(int64_t) * (i + 1));
+      CHECK_EQ(n, STREAM_LONG1::N_BYTES - TYPE_SIZE * (i + 1));
     }
   }
   //  --------------------------------------------------------------------------
   SUBCASE("[STREAM_LONG1] Incomplete stream") {
     auto *p = STREAM_LONG1::STREAM;
     // We induce an incomplete stream
-    auto n = STREAM_LONG1::N_BYTES - 2;
+    const auto STRM_LEN = STREAM_LONG1::N_BYTES - INCOMPLETE_OFFSET;
+    auto n = STRM_LEN;
 
     for (std::size_t i = 0; i < STREAM_LONG1::N_VALUES - 1; i++) {
       auto ret = parser.parse(p, n);
       CHECK_EQ(ret, ParseResult::ENDED);
       CHECK_EQ(parser.get(), STREAM_LONG1::VALUES[i]);
-      CHECK_EQ(n, STREAM_LONG1::N_BYTES - 2 - sizeof(int64_t) * (i + 1));
+      CHECK_EQ(n, STRM_LEN - TYPE_SIZE * (i + 1));
     }
 
     // Incomplete parsing
@@ -83,5 +89,84 @@ TEST_CASE("BytesParser<NBT::Long>") {
     CHECK_EQ(ret, ParseResult::UNFINISHED);
     CHECK_EQ(parser.get(), 0);
     CHECK_EQ(n, 0);
+  }
+}
+
+// ============================================================================
+TEST_CASE("BytesWriter<NBT::Long>") {
+
+  ByteWriter<INT_TYPE> dumper;
+  std::array<char, TYPE_SIZE> buffer;
+
+  //  --------------------------------------------------------------------------
+  SUBCASE("[LONG1] Normal case") {
+    dumper.set(LONG1::VALUE);
+    auto p = buffer.data();
+    std::size_t n = LONG1::N_BYTES;
+
+    auto ret = dumper.write(p, n);
+    check_writing_ended(ret, n);
+    buffers_are_equal(buffer, LONG1::BYTES, TYPE_SIZE);
+  }
+  //  --------------------------------------------------------------------------
+  SUBCASE("[LONG2] Negative case") {
+    dumper.set(LONG2::VALUE);
+    auto p = buffer.data();
+    auto n = LONG2::N_BYTES;
+    auto ret = dumper.write(p, n);
+    check_writing_ended(ret, n);
+    buffers_are_equal(buffer, LONG2::BYTES, TYPE_SIZE);
+  }
+  // --------------------------------------------------------------------------
+  SUBCASE("[LONG3] Big value case") {
+    dumper.set(LONG3::VALUE);
+    auto p = buffer.data();
+    auto n = LONG3::N_BYTES;
+    auto ret = dumper.write(p, n);
+    check_writing_ended(ret, n);
+    buffers_are_equal(buffer, LONG3::BYTES, TYPE_SIZE);
+  }
+  // --------------------------------------------------------------------------
+  SUBCASE("[STREAM_LONG1] Two longs") {
+    std::array<char, STREAM_LONG1::N_BYTES> long_buffer;
+    auto *p = long_buffer.data();
+    auto n = STREAM_LONG1::N_BYTES;
+
+    for (std::size_t i = 0; i < STREAM_LONG1::N_VALUES; i++) {
+      dumper.set(STREAM_LONG1::VALUES[i]);
+
+      auto ret = dumper.write(p, n);
+      CHECK_EQ(ret, WriteResult::ENDED);
+      CHECK_EQ(n, STREAM_LONG1::N_BYTES - TYPE_SIZE * (i + 1));
+    }
+    buffers_are_equal(long_buffer, STREAM_LONG1::STREAM, STREAM_LONG1::N_BYTES);
+  }
+  // --------------------------------------------------------------------------
+  SUBCASE("[STREAM_LONG1] Incomplete stream") {
+    std::array<char, STREAM_LONG1::N_BYTES> long_buffer;
+    long_buffer.fill('\x00');
+    auto *p = long_buffer.data();
+    // We induce an incomplete stream
+    const auto STRM_LEN = STREAM_LONG1::N_BYTES - INCOMPLETE_OFFSET;
+    auto n = STRM_LEN;
+
+    for (std::size_t i = 0; i < STREAM_LONG1::N_VALUES - 1; i++) {
+      dumper.set(STREAM_LONG1::VALUES[i]);
+      auto ret = dumper.write(p, n);
+      CHECK_EQ(ret, WriteResult::ENDED);
+      CHECK_EQ(n, STRM_LEN - TYPE_SIZE * (i + 1));
+    }
+
+    // Incomplete parsing
+    dumper.set(STREAM_LONG1::VALUES[STREAM_LONG1::N_VALUES - 1]);
+    auto ret = dumper.write(p, n);
+    CHECK_EQ(ret, WriteResult::UNFINISHED);
+    CHECK_EQ(n, 0);
+
+    // Check buffers
+    for (std::size_t i = 0; i < STRM_LEN; i++)
+      CHECK_EQ(long_buffer[i], STREAM_LONG1::STREAM[i]);
+    for (uint8_t i = STRM_LEN; i < STREAM_LONG1::N_VALUES * TYPE_SIZE; i++)
+      CHECK_EQ(long_buffer[i], 0);
   }
 }
